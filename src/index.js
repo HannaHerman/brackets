624551088876c201d3faab95e5eece0129ede2eb
HannaHerman <mrs.hanna.herman@gmail.com>
2020-10-18 14:06:13 +0300

    docs: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+  let arr = [];
+  let bracketsString = str.split('');
+  let config = Object.fromEntries(bracketsConfig);
+  for (let i = 0; i < bracketsString.length; i++) {
+    if (arr[0] === bracketsString[i]) {
+      arr.shift();
+    } else if (config[bracketsString[i]]) {
+      arr.unshift(config[bracketsString[i]]);
+    } else {
+      return false;
+    }
+  }
+  return arr.length === 0;
 }
